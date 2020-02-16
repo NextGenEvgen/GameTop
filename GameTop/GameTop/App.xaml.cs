ce63dc77532143f0678678d3f300a293201607ae
NextGenEvgen <57179288+NextGenEvgen@users.noreply.github.com>
2020-02-16 14:49:27 +0500

    Основная страница приложения

---
+++ GameTop/GameTop/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace GameTop
+{
+    public partial class App : Application
+    {
+        public static int ScreenHeight { get; set; }
+        public static int ScreenWidth { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = Color.Black,                
+            };            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
