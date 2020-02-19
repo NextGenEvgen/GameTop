10aad96db1ea355e3392ba63b41788029119f3df
NextGenEvgen <57179288+NextGenEvgen@users.noreply.github.com>
2020-02-19 21:14:26 +0500

    загрузка изображений с сервера

--- GameTop/MainActivity.cs
+++ GameTop/MainActivity.cs
@@ -4,14 +4,32 @@ using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Net;
+using Android.Graphics;
+using Java.IO;
+using Android.Graphics.Drawables;
+using Android.Util;
+using System.Net;
+using System.IO;
 namespace GameTop
 {
     [Activity(Label = "GameTop", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
-        Button button;
+        //ImageButton button;
 
-        int counter = 0;
+        //int counter = 0;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -20,13 +38,32 @@ namespace GameTop
 
             SetContentView(Resource.Layout.activity_main);
 
-            button = FindViewById<Button>(Resource.Id.button);
-            button.Click += Button_Click;
+            FindViewById<ImageView>(Resource.Id.imageView1).SetImageBitmap(GetImageFromUrl("http://192.168.1.104:8080/Dark%20Souls.png"));
+            //image.SetImageBitmap(GetImageFromUrl("https://i.redd.it/lb9h2bfqnuh41.png"));
+            //Android.Net.Uri url = Android.Net.Uri.Parse("https://i.redd.it/lb9h2bfqnuh41.png");
+            //image.SetImageURI(url);
+
+            //ImageButton button = FindViewById<ImageButton>(Resource.Id.imageButton1);
+            //button.SetImageURI(Android.Net.Uri.Parse("https://zhitanska.com/wp-content/uploads/2019/01/rani-ki-vav-11.jpg"));
+        }
+
+        private Bitmap GetImageFromUrl(string url)
+        {
+            Bitmap image = null;
+            using (var webClient = new WebClient())
+            {
+                var bytes = webClient.DownloadData(url);
+                if (bytes != null && bytes.Length > 0)
+                {
+                    image = BitmapFactory.DecodeByteArray(bytes, 0, bytes.Length);
+                }
+            }
+            return image;
         }
 
         private void Button_Click(object sender, System.EventArgs e)
         {
-            button.Text = $"Вы нажали {counter++} раз";
+            //button.SetImageURI()
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
